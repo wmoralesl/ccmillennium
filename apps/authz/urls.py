68bebdf0a667386b1a1d14f1d24d3a7a55ec325f
Wilson Morales <wilsonm57wm@gmail.com>
2025-04-08 11:23:59 -0600

    [Update]: Se ha agregado el boton para cerrar sesion

--- apps/authz/urls.py
+++ apps/authz/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, link_logout
 
 app_name = 'auth'
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', link_logout, name='logout'),
+
 ]
