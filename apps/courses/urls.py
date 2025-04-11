b0e3c580e50aac40c91e7b1c06d4d3233554435e
Wilson Morales <wilsonm57wm@gmail.com>
2025-04-11 14:06:34 -0600

    [Update]: Completando Rutas

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import CourseListView, CourseCreateView
+from .views import CourseListView, CourseCreateView, CourseEditView, CourseDetailView
 
 app_name = 'courses'
 
 urlpatterns = [
     path('', CourseListView.as_view(), name='list'),
     path('create/', CourseCreateView.as_view(), name='create'),
+    path('edit/<int:pk>/', CourseEditView.as_view(), name='edit'),
+    path('view/<int:pk>/', CourseDetailView.as_view(), name='view'),
 ]
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DetailView
 from courses.models import Course
 from courses.forms import CourseCreateForm
 from django.urls import reverse, reverse_lazy
@@ -16,3 +16,16 @@ class CourseCreateView(CreateView):
     form_class = CourseCreateForm
     template_name = 'courses/form_course.html'
     success_url = reverse_lazy('courses:list')
+
+class CourseEditView(UpdateView):
+    model = Course
+    form_class = CourseCreateForm
+    template_name = 'courses/form_course.html'
+
+    def get_success_url(self):
+        return reverse('courses:view', kwargs={'pk': self.object.id})
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/detail_course.html'
+    context_object_name = 'course'
