f0ef8d11b6a09f4cc2c1968c7a6a07eb826ee5ad
Wilson Morales <wilsonm57wm@gmail.com>
2025-04-15 09:58:29 -0600

    [Update]: Se ha agregado el modelo de grupos

--- apps/groups/models.py
+++ apps/groups/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    teacher = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        limit_choices_to={'role': 'teacher'},
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    year = models.PositiveIntegerField()
+    in_person = models.BooleanField(default=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    schedule = models.TimeField()  # Start time of the group
+    hours_count = models.PositiveIntegerField()
+    is_visible = models.BooleanField(default=True)
+    course = models.ForeignKey(
+        'courses.Course',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.name} ({self.year})'
